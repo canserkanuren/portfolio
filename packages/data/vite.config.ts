52c5028887f9ec9c8e0cf98e650eeafe298e63a5
canserkanuren <canserkan.uren@gmail.com>
2025-04-25 20:17:15 +0200

    fix(data): update tsconfigPath variable property in vite config

--- packages/data/vite.config.ts
+++ packages/data/vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types='vitest' />
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import * as path from 'path';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
@@ -12,8 +12,7 @@ export default defineConfig({
     nxViteTsPaths(),
     dts({
       entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json')
     })
   ],
 
